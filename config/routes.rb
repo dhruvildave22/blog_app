4212a3da11a81b4b9d54e26e14e8d66870387334
dhruvildave <dhruvildave22@gmail.com>
2019-05-13 15:39:08 +0530

    signup and login completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   resources :articles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+
 end
